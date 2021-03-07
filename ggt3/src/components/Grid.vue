2f1f2b62cf638515f2d0759852d22b1e19d35097
Grace Gude <gracegude@BOOJ0000658-1001.local>
2021-03-06 20:36:13 -0700

    Add initial vue app

---
+++ ggt3/src/components/Grid.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Tic Tac Toe</h1>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Grid'
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ ggt3/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Grid msg="Tic Tac Toe"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Grid from '@/components/Grid.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Grid
+  }
+}
+</script>
